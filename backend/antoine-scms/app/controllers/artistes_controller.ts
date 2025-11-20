7df00fe3abc6819030cf4f22920a2020ff8be04a
Antoine <163823977+antoineFabr@users.noreply.github.com>
2025-11-20 11:36:07 +0100

    Feat: Ajout de la route /api/artiste/:id pour recuperer un artiste avec son id

--- backend/antoine-scms/app/controllers/artistes_controller.ts
+++ backend/antoine-scms/app/controllers/artistes_controller.ts
@@ -29,7 +29,22 @@ export default class ArtistesController {
       return response.status(500).send("erreur de création d'artiste")
     }
   }
-  async getById({}: HttpContext) {}
+  async getById({ params, response }: HttpContext) {
+    const id = params.id
+
+    try {
+      if (isNaN(Number(id))) {
+        return response.status(404).send("l'id de l'artiste doit etre un nombre")
+      }
+      const artiste = await Artiste.findBy('id', id)
+      if (!artiste) {
+        return response.status(404).send('artiste pas trouvé')
+      }
+      return response.status(200).send(artiste)
+    } catch (err) {
+      logger.error({ err: err }, `erreur lors de la récuperation de l'artiste ${id}`)
+    }
+  }
   async modify({}: HttpContext) {}
   async delete({}: HttpContext) {}
 }
