0ac7465281ad950dfff5e0a84fa9dc881f5f676b
Antoine <163823977+antoineFabr@users.noreply.github.com>
2025-11-20 17:11:19 +0100

    Feat: ajout de la page de login et de la page d'admin

--- frontend/antoine-scms/app/(auth)/login/page.tsx
+++ frontend/antoine-scms/app/(auth)/login/page.tsx
@@ -1,7 +1,71 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+type FormData = { mdp: string; mail: string };
 export default function Login() {
+  const router = useRouter();
+  const [form, setForm] = useState<FormData>({ mdp: "", mail: "" });
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((x) => ({ ...x, [name]: value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    if (!form.mail.trim() || !form.mdp) {
+      setError("Il faut remplir les champs obligatoires");
+    }
+
+    const response = await fetch("http://localhost:3333/api/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: form.mail.trim(),
+        password: form.mdp,
+      }),
+    });
+
+    if (response.status != 200) {
+      setError("Erreur de création de compte !");
+      setLoading(false);
+      return;
+    }
+    setLoading(false);
+    router.push("/admin");
+  };
+
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <form onSubmit={handleSubmit} noValidate>
+      <h1>Créer votre compte</h1>
+
+      <p className="mt-1 text-red-400 text-sm">{error}</p>
+      <input
+        type="email"
+        name="mail"
+        placeholder="email"
+        value={form.mail}
+        onChange={handleChange}
+        required
+      />
+      <input
+        type="password"
+        name="mdp"
+        placeholder="mot de passe"
+        value={form.mdp}
+        onChange={handleChange}
+        required
+      />
+
+      <button type="submit" disabled={loading} className="">
+        {loading ? "Connexion en cours" : "Connexion"}
+      </button>
+    </form>
   );
 }
